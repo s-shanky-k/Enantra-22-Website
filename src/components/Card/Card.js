581259d7e3ca0ff02792fbac6399694133ba101b
Shankar Kumar S <seshankar20@gmail.com>
2022-03-18 01:07:51 +0530

    Added mobile responsivness for Card component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,20 +1,29 @@
 import React from "react";
 import styles from "./Card.module.css";
 
-function Card() {
+// Usage:
+// <Card
+//     props={{
+//         img: "netflixlogo.jpg",
+//         title: "Title",
+//         description: "This is the description about the event",
+//     }}
+// />
+
+function Card({ props }) {
 	return (
 		<>
 			<div className={styles.cardContainer}>
 				<div className={styles.imgContainer}>
 					<img
 						className={styles.img}
-						src={`${process.env.PUBLIC_URL}/images/netflixlogo.jpg`}
+						src={`${process.env.PUBLIC_URL}/images/${props.img}`}
 					></img>
 				</div>
 				<div className={styles.textContainer}>
-					<div className={styles.title}>Title</div>
+					<div className={styles.title}>{props.title}</div>
 					<div className={styles.description}>
-						This is the description about the event
+						{props.description}
 					</div>
 				</div>
 			</div>
