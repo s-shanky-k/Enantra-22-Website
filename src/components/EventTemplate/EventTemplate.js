803bcf39a213731eea4be3e039c93db7c069c05d
Shankar Kumar S <seshankar20@gmail.com>
2022-03-19 01:15:08 +0530

    Updated EventTemplate

--- src/components/EventTemplate/EventTemplate.js
+++ src/components/EventTemplate/EventTemplate.js
@@ -5,42 +5,55 @@ import TextCard from "../TextCard/TextCard";
 import {
 	faCalendarDay,
 	faLocationDot,
+	faClock,
+	faIndianRupeeSign,
 } from "@fortawesome/free-solid-svg-icons";
 
-function EventTemplate() {
+function EventTemplate({ props }) {
 	return (
 		<div className={styles.eventTemplateContainer}>
 			<div className={styles.eventDetails}>
 				<div className={styles.eventImgContainer}>
 					<img
 						className={styles.eventImg}
-						src={`${process.env.PUBLIC_URL}/images/netflixlogo.jpg`}
+						src={`${process.env.PUBLIC_URL}/images/${props.img}`}
 					></img>
 				</div>
 				<div className={styles.eventDateTimeVenue}>
-					<div className={styles.eventDateTime}>
+					<div className={styles.eventTime}>
+						<FontAwesomeIcon
+							icon={faClock}
+							className={styles.eventIcons}
+						/>
+						{props.time}
+					</div>
+					<div className={styles.eventDate}>
 						<FontAwesomeIcon
 							icon={faCalendarDay}
 							className={styles.eventIcons}
 						/>
-						23rd March, 2022
+						{props.date}
 					</div>
 					<div className={styles.eventVenue}>
 						<FontAwesomeIcon
 							icon={faLocationDot}
 							className={styles.eventIcons}
 						/>
-						CEG, Anna University
+						{props.venue}
 					</div>
 				</div>
-				<button>REGISTER</button>
+				<button>
+					Register-
+					<FontAwesomeIcon icon={faIndianRupeeSign} />
+					{props.fee}
+				</button>
 			</div>
 			<div className={styles.eventDescription}>
 				<TextCard
 					props={{
-						title: "Event Name",
-						description:
-							"This is a mega monster event. This is a mega monster event. This is a mega monster event. This is a mega monster event. This is a mega monster event. This is a mega monster event.",
+						title: props.title,
+						description: props.description,
+						contact: props.contact,
 					}}
 				/>
 			</div>
