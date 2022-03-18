cb6306b22b5c5ef4082dfcbe4d00ca55b612a0bb
Shankar Kumar S <seshankar20@gmail.com>
2022-03-19 02:22:37 +0530

    Added WorkshopTemplate

---
+++ src/components/WorkshopTemplate/WorkshopTemplate.js
@@ -0,0 +1,89 @@
+import React from "react";
+import styles from "./WorkshopTemplate.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import TextCard from "../TextCard/TextCard";
+import {
+	faCalendarDay,
+	faLocationDot,
+	faClock,
+	faIndianRupeeSign,
+} from "@fortawesome/free-solid-svg-icons";
+import ImageCard from "../ImageCard/ImageCard";
+
+// Usage:
+{/* <WorkshopTemplate
+	props={{
+		img: "netflixlogo.jpg",
+		workshop_title: "Workshop Name",
+		workshop_description:
+			"This is the description about the workshop. This a ultra mass OP Max workshop. This a ultra mass OP Max workshop. This a ultra mass OP Max workshop. This the description about the workshop. This is the description about the workshop",
+		time: "2:00 PM",
+		date: "23rd March, 2022",
+		venue: "CEG, Anna University",
+		fee: "1000",
+		contact: "Shankar - 6969696969",
+		href: "https://www.youtube.com/watch?v=8FAUEv_E_xQ&ab_channel=SunTV",
+		organizer_title:"Organizer",
+		organizer_img:"netflixlogo.jpg",
+		organizer_description:"This is a boomer"
+	}} */}
+// />;
+
+function WorkshopTemplate({ props }) {
+	return (
+		<div className={styles.workshopTemplateContainer}>
+			<div className={styles.workshopDetails}>
+				<div className={styles.workshopImgContainer}>
+					<img
+						className={styles.workshopImg}
+						src={`${process.env.PUBLIC_URL}/images/${props.img}`}
+					></img>
+				</div>
+				<div className={styles.workshopDateTimeVenue}>
+					<div className={styles.workshopTime}>
+						<FontAwesomeIcon
+							icon={faClock}
+							className={styles.workshopIcons}
+						/>
+						{props.time}
+					</div>
+					<div className={styles.workshopDate}>
+						<FontAwesomeIcon
+							icon={faCalendarDay}
+							className={styles.workshopIcons}
+						/>
+						{props.date}
+					</div>
+					<div className={styles.workshopVenue}>
+						<FontAwesomeIcon
+							icon={faLocationDot}
+							className={styles.workshopIcons}
+						/>
+						{props.venue}
+					</div>
+				</div>
+				<a href={props.href} target="_blank" rel="noopener noreferrer">
+					Register-
+					<FontAwesomeIcon icon={faIndianRupeeSign} />
+					{props.fee}
+				</a>
+			</div>
+			<div className={styles.workshopDescription}>
+				<TextCard
+					props={{
+						title: props.workshop_title,
+						description: props.workshop_description,
+						contact: props.workshop_contact,
+					}}
+				/>
+				<ImageCard props={{
+						title: props.organizer_title,
+						description: props.organizer_description,
+						img: props.organizer_img,
+					}}/>
+			</div>
+		</div>
+	);
+}
+
+export default WorkshopTemplate;
