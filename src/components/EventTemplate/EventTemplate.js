721a0162ac1ff5940a80950266065ae0d40a5ccc
Shankar Kumar S <seshankar20@gmail.com>
2022-03-19 01:27:59 +0530

    Minor fixes

--- src/components/EventTemplate/EventTemplate.js
+++ src/components/EventTemplate/EventTemplate.js
@@ -9,6 +9,22 @@ import {
 	faIndianRupeeSign,
 } from "@fortawesome/free-solid-svg-icons";
 
+// Usage:
+// <EventTemplate
+// 	props={{
+// 		img: "netflixlogo.jpg",
+// 		title: "Event Name",
+// 		description:
+// 			"This is the description about the event. This a ultra mass OP Max event. This a ultra mass OP Max event. This a ultra mass OP Max event. This the description about the event. This is the description about the event",
+// 		time: "2:00 PM",
+// 		date: "23rd March, 2022",
+// 		venue: "CEG, Anna University",
+// 		fee: "1000",
+// 		contact: "Shankar - 6969696969",
+// 		href: "https://www.youtube.com/watch?v=8FAUEv_E_xQ&ab_channel=SunTV",
+// 	}}
+// />;
+
 function EventTemplate({ props }) {
 	return (
 		<div className={styles.eventTemplateContainer}>
@@ -42,11 +58,11 @@ function EventTemplate({ props }) {
 						{props.venue}
 					</div>
 				</div>
-				<button>
+				<a href={props.href} target="_blank" rel="noopener noreferrer">
 					Register-
 					<FontAwesomeIcon icon={faIndianRupeeSign} />
 					{props.fee}
-				</button>
+				</a>
 			</div>
 			<div className={styles.eventDescription}>
 				<TextCard
