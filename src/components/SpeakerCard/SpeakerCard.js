dbc7e1ea37cfa3b79e9cfba5d9dd2b0b24bdfc29
Shankar Kumar S <seshankar20@gmail.com>
2022-03-19 17:03:40 +0530

    Added SpeakerCard component base code

---
+++ src/components/SpeakerCard/SpeakerCard.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from './SpeakerCard.module.css'
+
+function SpeakerCard() {
+  return (
+    <div className={styles.container}>
+  <div className={styles.card}>
+    <div className={styles.cardImg}>
+      <img src={`${process.env.PUBLIC_URL}/images/netflixlogo.jpg`} />
+    </div>
+    <div className={styles.cardContent}>
+      <h2 className={styles.bigTitle}>Kishan Das</h2>
+      <h3 className={styles.mediumTitle}>Noob</h3>
+    </div>
+  </div>
+</div>
+  )
+}
+
+export default SpeakerCard
