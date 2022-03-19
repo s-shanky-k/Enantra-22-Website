3e871679566b81f9abf561c34c20e2096991a02b
Paargav Shanker <paargav.shuttle@gmail.com>
2022-03-19 23:41:22 +0530

    6dt initial design

--- src/pages/SixDTTalks/SixDTTalks.js
+++ src/pages/SixDTTalks/SixDTTalks.js
@@ -1,24 +1,28 @@
 import React from "react";
 import styles from "./SixDTTalks.module.css";
 import SpeakerCard from "../../components/SpeakerCard/SpeakerCard";
-import { SixDTTalksData } from "../../assets/data/SixDTTalksData";
 import Button from "../../components/Button/Button";
+import Heading from "../../components/Heading/Heading";
+import { SixDTTalksData } from "../../data/SixDTTalks";
 
 function SixDTTalks() {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.headingContainer}>
-        <h1>6DT</h1>
-      </div>
+      <div className={styles.bgDiv}></div>
+      <div className={styles.innerWrapper}>
+        <div className={styles.headingContainer}>
+          <Heading text={"6DT"} />
+        </div>
 
-      <div className={styles.speakersContainer}>
-        {SixDTTalksData.map((item, index) => (
-          <SpeakerCard key={index + 1} props={item} />
-        ))}
-      </div>
+        <div className={styles.speakersContainer}>
+          {SixDTTalksData.map((item, index) => (
+            <SpeakerCard key={index + 1} props={item} />
+          ))}
+        </div>
 
-      <div className={styles.buttonContainer}>
-        <Button text={"Register"} />
+        <div className={styles.buttonContainer}>
+          <Button text={"Register"} />
+        </div>
       </div>
     </div>
   );
