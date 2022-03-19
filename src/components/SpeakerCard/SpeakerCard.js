8215211a58c721a79ea7eaab5e7c4b87f4647eb0
Shankar Kumar S <seshankar20@gmail.com>
2022-03-19 17:41:56 +0530

    Minor fix

--- src/components/SpeakerCard/SpeakerCard.js
+++ src/components/SpeakerCard/SpeakerCard.js
@@ -1,20 +1,22 @@
-import React from 'react'
-import styles from './SpeakerCard.module.css'
+import React from "react";
+import styles from "./SpeakerCard.module.css";
 
 function SpeakerCard() {
-  return (
-    <div className={styles.container}>
-  <div className={styles.card}>
-    <div className={styles.cardImg}>
-      <img src={`${process.env.PUBLIC_URL}/images/netflixlogo.jpg`} />
-    </div>
-    <div className={styles.cardContent}>
-      <h2 className={styles.bigTitle}>Kishan Das</h2>
-      <h3 className={styles.mediumTitle}>Noob</h3>
-    </div>
-  </div>
-</div>
-  )
+	return (
+		<div className={styles.container}>
+			<div className={styles.card}>
+				<div className={styles.cardImg}>
+					<img
+						src={`${process.env.PUBLIC_URL}/images/netflixlogo.jpg`}
+					/>
+				</div>
+				<div className={styles.cardContent}>
+					<h2 className={styles.bigTitle}>Kishan Das</h2>
+					<h3 className={styles.mediumTitle}>Noob</h3>
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default SpeakerCard
+export default SpeakerCard;
