eccea9c6e6e6e981811b88dc585ef7daf9a18e24
Shankar Kumar S <seshankar20@gmail.com>
2022-03-19 17:57:04 +0530

    Fixed error

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,18 +1,10 @@
 import React from "react";
 import styles from "./Landing.module.css";
-import Card from "../../components/Card/Card";
+
 function Landing() {
 	return (
 		<>
 			<div>Landing</div>
-			<Card
-				props={{
-					img: "netflixlogo.jpg",
-					title: "TitleTitleTitleTitleTitleTitleTitle",
-					description:
-						"This is the description about the event. This is the description about the event. This is the description about the event",
-				}}
-			/>
 		</>
 	);
 }
