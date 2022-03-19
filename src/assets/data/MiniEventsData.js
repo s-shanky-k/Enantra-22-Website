a1a4f1dbc3f538f83daf7be4430a1b706bd0a7f0
Shankar Kumar S <seshankar20@gmail.com>
2022-03-19 20:41:17 +0530

    Added SixDTTalks data

---
+++ src/assets/data/MiniEventsData.js
@@ -0,0 +1,8 @@
+export const MiniEventsData = [
+	{
+		img: "netflixlogo.jpg",
+		title: "Title",
+		description: "This is the description about the event",
+		href: "/events",
+	},
+];
---
+++ src/assets/data/SixDTTalksData.js
@@ -0,0 +1,27 @@
+export const SixDTTalksData = [
+	{
+		img: "netflixlogo.jpg",
+		title: "Kishen Das",
+		description: "Actor and Entrepreneur ",
+	},
+	{
+		img: "netflixlogo.jpg",
+		title: "Sarath Kumar",
+		description: "Social Media Influencer ",
+	},
+	{
+		img: "netflixlogo.jpg",
+		title: "M S Krsna",
+		description: "Musician",
+	},
+	{
+		img: "netflixlogo.jpg",
+		title: "Akash, Sanjana and Darshini",
+		description: "Founders of Vizha ",
+	},
+	{
+		img: "netflixlogo.jpg",
+		title: "Santosh Muruganathan",
+		description: "Co-founder of Kolapasi",
+	},
+];
