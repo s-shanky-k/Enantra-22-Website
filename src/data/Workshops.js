30f3f41fb4920ac1b580c9e2a9c6699302ffb24b
Shankar Kumar S <seshankar20@gmail.com>
2022-03-20 12:05:53 +0530

    Fixed card issues

--- src/data/Workshops.js
+++ src/data/Workshops.js
@@ -1,109 +1,113 @@
 const imageBasePath = `${process.env.PUBLIC_URL}/assets/images/Workshops/`;
 
 export const WORKSHOPS = [
-  // Automotive Industry
-  {
-    title: "Emerging Trends in Automotive Industry and Autonomous Cars",
-    url: "automotive-trends",
-    img: imageBasePath + "automotive-trends/netflixlogo.jpg",
-    speakers: [
-      {
-        name: "Mr.Boopathy Srinivasan",
-        company: "NIFT, Chennai",
-        image: imageBasePath + "automotive-trends/netflixlogo.jpg",
-      },
-    ],
-    time: "10am onwards",
-    date: "31/3/2022 and 1/4/2022",
-    venue: "HM Hall",
-    oneLiner: "Learn how to become an entrepreneur in Automotive service",
-    description:
-      "Learn how to become an entrepreneur in Automotive service. Obtain in depth experience with the latest control systems and electronic components in cars. Explore the future of Autonomous cars, electric vehicles, hybrid vehicles and hydrogen cell cars and its practical applications with hands on training session.",
-    regFee: "Rs. 799",
-    contact: [
-      {
-        name: "Shirapthi",
-        mobile: "+91 93612 80902",
-      },
-    ],
-  },
+	// Automotive Industry
+	{
+		title: "Emerging Trends in Automotive Industry and Autonomous Cars",
+		url: "automotive-trends",
+		img: imageBasePath + "automotive-trends/netflixlogo.jpg",
+		speakers: [
+			{
+				name: "Mr.Boopathy Srinivasan",
+				company: "NIFT, Chennai",
+				image: imageBasePath + "automotive-trends/netflixlogo.jpg",
+			},
+		],
+		time: "10am onwards",
+		date: "31/3/2022 and 1/4/2022",
+		venue: "HM Hall",
+		oneLiner:
+			"Learn how to become an entrepreneur in Automotive service. Explore the future of Autonomous, electric, hybrid and hydrogen cell cars and its practical applications with hands on training session.",
+		description:
+			"Learn how to become an entrepreneur in Automotive service. Obtain in depth experience with the latest control systems and electronic components in cars. Explore the future of Autonomous cars, electric vehicles, hybrid vehicles and hydrogen cell cars and its practical applications with hands on training session.",
+		regFee: "Rs. 799",
+		contact: [
+			{
+				name: "Shirapthi",
+				mobile: "+91 93612 80902",
+			},
+		],
+	},
 
-  // Film Making
-  {
-    title: "Art of Film Making and Story Telling ",
-    url: "film-making",
-    img: imageBasePath + "film-making/logo.png",
-    speakers: [
-      {
-        name: "Mr. SV Roshan",
-        company: "MGR Film and TV Institute",
-        image: imageBasePath + "film-making/logo.png",
-      },
-    ],
-    time: "9am - 5pm ",
-    date: "TBD",
-    venue: "RUSA Galley",
-    oneLiner: "Learn how to become an entrepreneur in Automotive service",
-    description:
-      "Learn more about the real process behind making of a film, use of sound in films and cinematography. Get Hands on training for use of lights in cinematography. Interactive group discussions on Behind the scenes and making of popular films. Fun and interesting activities on development of a story.",
-    regFee: "Rs. 499",
-    contact: [
-      {
-        name: "Nivedita",
-        mobile: "+91 63848 56600",
-      },
-    ],
-  },
+	// Film Making
+	{
+		title: "Art of Film Making and Story Telling ",
+		url: "film-making",
+		img: imageBasePath + "film-making/logo.png",
+		speakers: [
+			{
+				name: "Mr. SV Roshan",
+				company: "MGR Film and TV Institute",
+				image: imageBasePath + "film-making/logo.png",
+			},
+		],
+		time: "9am - 5pm ",
+		date: "TBD",
+		venue: "RUSA Galley",
+		oneLiner:
+			"Learn more about the real process behind making of a film, use of sounds in films and about cinematography.",
+		description:
+			"Learn more about the real process behind making of a film, use of sounds in films and cinematography. Get Hands on training for use of lights in cinematography. Interactive group discussions on Behind the scenes and making of popular films. Fun and interesting activities on development of a story.",
+		regFee: "Rs. 499",
+		contact: [
+			{
+				name: "Nivedita",
+				mobile: "+91 63848 56600",
+			},
+		],
+	},
 
-  // Stock Market
-  {
-    title: "Stock Market and Investing",
-    url: "stock-market",
-    img: imageBasePath + "stock-market/logo.png",
-    speakers: [
-      {
-        name: "Mr. Ravi Padmanabhan",
-        image: imageBasePath + "stock-market/netflixlogo.jpg",
-      },
-    ],
-    time: "10am onwards",
-    date: "31/3/2022 and 1/4/2022",
-    venue: "Ocean Management Department",
-    oneLiner: "Learn how to become an entrepreneur in Automotive service",
-    description:
-      "Gain complete understanding and confidence when investing in the Stock Market. Apply key stock investing strategies such as investing in Dividend Paying Stocks, Growth Stocks & Value Stocks. Understand how to read market cycles and overcome the fear of losing money.",
-    regFee: "Rs. 499",
-    contact: [
-      {
-        name: "Jebin",
-        mobile: "+91 63828 03541",
-      },
-    ],
-  },
+	// Stock Market
+	{
+		title: "Stock Market and Investing",
+		url: "stock-market",
+		img: imageBasePath + "stock-market/logo.png",
+		speakers: [
+			{
+				name: "Mr. Ravi Padmanabhan",
+				image: imageBasePath + "stock-market/netflixlogo.jpg",
+			},
+		],
+		time: "10am onwards",
+		date: "31/3/2022 and 1/4/2022",
+		venue: "Ocean Management Department",
+		oneLiner:
+			"Gain complete understanding and confidence when investing in the Stock Market. Learn about investing strategies.",
+		description:
+			"Gain complete understanding and confidence when investing in the Stock Market. Apply key stock investing strategies such as investing in Dividend Paying Stocks, Growth Stocks & Value Stocks. Understand how to read market cycles and overcome the fear of losing money.",
+		regFee: "Rs. 499",
+		contact: [
+			{
+				name: "Jebin",
+				mobile: "+91 63828 03541",
+			},
+		],
+	},
 
-  // The Metaverse
-  {
-    title: "The Metaverse",
-    url: "metaverse",
-    img: imageBasePath + "metaverse/logo.png",
-    speakers: [
-      {
-        name: "XR Labs",
-        image: imageBasePath + "metaverse/netflixlogo.jpg",
-      },
-    ],
-    time: "9am - 5pm",
-    date: "TBD",
-    venue: "ECE Departmen",
-    oneLiner: "Learn how to become an entrepreneur in Automotive service",
-    description:
-      "Corona has made a huge impact on work environment. The whole office, college and school have shrunk into a laptop and phone. Now it’s time for a revolution to bring a new world into one’s home. Want to know more about Facebook’s new name ‘META’?! Want to be the creator of the Meta universe?! Here is the right opportunity for you to learn metaverse from scratch through group discussions and fun interactive sessions. XR Labs is digital experiences company that has done significant work in Augmented Reality, Virtual reality and mixed reality and also 100+ successful projects.",
-    regFee: "Rs. 599",
-    contact: [
-      {
-        name: "Name needed",
-        mobile: "+91 73583 31932",
-      },
-    ],
-  },
+	// The Metaverse
+	{
+		title: "The Metaverse",
+		url: "metaverse",
+		img: imageBasePath + "metaverse/logo.png",
+		speakers: [
+			{
+				name: "XR Labs",
+				image: imageBasePath + "metaverse/netflixlogo.jpg",
+			},
+		],
+		time: "9am - 5pm",
+		date: "TBD",
+		venue: "ECE Department",
+		oneLiner:
+			"Learn about the metaverse from scratch through group discussions and fun interactive sessions.",
+		description:
+			"Corona has made a huge impact on work environment. The whole office, college and school have shrunk into a laptop and phone. Now it’s time for a revolution to bring a new world into one’s home. Want to know more about Facebook’s new name ‘META’?! Want to be the creator of the Meta universe?! Here is the right opportunity for you to learn metaverse from scratch through group discussions and fun interactive sessions. XR Labs is digital experiences company that has done significant work in Augmented Reality, Virtual reality and mixed reality and also 100+ successful projects.",
+		regFee: "Rs. 599",
+		contact: [
+			{
+				name: "Name needed",
+				mobile: "+91 73583 31932",
+			},
+		],
+	},
 ];
