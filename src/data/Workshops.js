e035aeb88e1ce6a64aa87c9077c61cf6431e3ee3
Shankar Kumar S <seshankar20@gmail.com>
2022-03-21 23:48:53 +0530

    Minor fix

--- src/data/Workshops.js
+++ src/data/Workshops.js
@@ -1,171 +1,173 @@
 const imageBasePath = `${process.env.PUBLIC_URL}/assets/images/Workshops/`;
 
 export const WORKSHOPS = [
-  // Automotive Industry
-  {
-    title: "Emerging Trends in Automotive Industry and Autonomous Cars",
-    url: "automotive-trends",
-    img: imageBasePath + "automotive-trends/logo.png",
-    speakers: [
-      {
-        name: "GOODWIN MOTORS",
-        company: "A BMW Skill Initiative Partner",
-        image: imageBasePath + "automotive-trends/goodwin.png",
-      },
-    ],
-    time: "10:00 AM",
-    date: "March 31, 2022 and April 1, 2022",
-    venue: "HM Hall",
-    oneLiner: "Learn how to become an entrepreneur in Automotive service",
-    description:
-      "Learn how to become an entrepreneur in Automotive service. Obtain in depth experience with the latest control systems and electronic components in cars. Explore the future of Autonomous cars, electric vehicles, hybrid vehicles and hydrogen cell cars and its practical applications with hands on training session.",
-    regFee: "₹799 (including taxes)",
-    contact: [
-      {
-        name: "Dharaneesh",
-        mobile: "+91 95859 61212",
-      },
-    ],
-  },
+	// Automotive Industry
+	{
+		title: "Emerging Trends in Automotive Industry and Autonomous Cars",
+		url: "automotive-trends",
+		img: imageBasePath + "automotive-trends/logo.png",
+		speakers: [
+			{
+				name: "GOODWIN MOTORS",
+				company: "A BMW Skill Initiative Partner",
+				image: imageBasePath + "automotive-trends/goodwin.png",
+			},
+		],
+		time: "10:00 AM",
+		date: "March 31, 2022 and April 1, 2022",
+		venue: "HM Hall",
+		oneLiner:
+			"Learn how to become an entrepreneur in Automotive service. Explore the future of autonomous, electric, hybrid and hydrogen cell cars and its practical applications with hands on training session.",
+		description:
+			"Learn how to become an entrepreneur in Automotive service. Obtain in depth experience with the latest control systems and electronic components in cars. Explore the future of Autonomous cars, electric vehicles, hybrid vehicles and hydrogen cell cars and its practical applications with hands on training session.",
+		regFee: "₹799 (including taxes)",
+		contact: [
+			{
+				name: "Dharaneesh",
+				mobile: "+91 95859 61212",
+			},
+		],
+	},
 
-  // Film Making
-  {
-    title: "Art of Film Making and Story Telling ",
-    url: "film-making",
-    img: imageBasePath + "film-making/logo.png",
-    speakers: [
-      {
-        name: "SV Roshan",
-        company:
-          "A senior student of MGR Film and Television Institute Taramani",
-        image: imageBasePath + "film-making/logo.png",
-      },
-    ],
-    time: "9:00 AM",
-    date: "March 31, 2022",
-    venue: "RUSA Galley",
-    oneLiner:
-      "Learn more about the real process behind making of a film, use of sounds in films and about cinematography.",
-    description:
-      "Learn more about the real process behind making of a film, use of sound in films and cinematography. Get Hands on training for use of lights in cinematography. Interactive group discussions on Behind the scenes and making of popular films. Fun and interesting activities on development of a story.",
-    regFee: "₹499 (including taxes)",
-    contact: [
-      {
-        name: "Akshay",
-        mobile: "+91 98410 44390",
-      },
-    ],
-  },
+	// Film Making
+	{
+		title: "Art of Film Making and Story Telling ",
+		url: "film-making",
+		img: imageBasePath + "film-making/logo.png",
+		speakers: [
+			{
+				name: "SV Roshan",
+				company:
+					"A senior student of MGR Film and Television Institute Taramani",
+				image: imageBasePath + "film-making/logo.png",
+			},
+		],
+		time: "9:00 AM",
+		date: "March 31, 2022",
+		venue: "RUSA Galley",
+		oneLiner:
+			"Learn more about the real process behind making of a film, use of sounds in films and about cinematography. ",
+		description:
+			"Learn more about the real process behind making of a film, use of sound in films and cinematography. Get Hands on training for use of lights in cinematography. Interactive group discussions on Behind the scenes and making of popular films. Fun and interesting activities on development of a story.",
+		regFee: "₹499 (including taxes)",
+		contact: [
+			{
+				name: "Akshay",
+				mobile: "+91 98410 44390",
+			},
+		],
+	},
 
-  // Stock Market
-  {
-    title: "Stock Market and Investing",
-    url: "stock-market",
-    img: imageBasePath + "stock-market/logo.png",
-    speakers: [
-      {
-        name: "Mr. Ravi Padmanabhan",
-        company: "Wealth creation expert and founder of art of wise investing",
-        image: imageBasePath + "stock-market/logo.png",
-      },
-    ],
-    time: "10:00-12:00 & 1:00-4:00",
-    date: "March 31, 2022 and April 1, 2022",
-    venue: "Ocean Management Department",
-    oneLiner:
-      "Gain complete understanding and confidence when investing in the Stock Market. Learn about investing strategies.",
-    description:
-      "Gain complete understanding and confidence when investing in the Stock Market. Apply key stock investing strategies such as investing in Dividend Paying Stocks, Growth Stocks & Value Stocks. Understand how to read market cycles and overcome the fear of losing money.",
-    regFee: "₹499",
-    contact: [
-      {
-        name: "Nehal",
-        mobile: "+91 99417 50200",
-      },
-    ],
-  },
+	// Stock Market
+	{
+		title: "Stock Market and Investing",
+		url: "stock-market",
+		img: imageBasePath + "stock-market/logo.png",
+		speakers: [
+			{
+				name: "Mr. Ravi Padmanabhan",
+				company:
+					"Wealth creation expert and founder of art of wise investing",
+				image: imageBasePath + "stock-market/logo.png",
+			},
+		],
+		time: "10:00-12:00 & 1:00-4:00",
+		date: "March 31, 2022 and April 1, 2022",
+		venue: "Ocean Management Department",
+		oneLiner:
+			"Gain complete understanding and confidence when investing in the Stock Market. Learn about investing strategies.",
+		description:
+			"Gain complete understanding and confidence when investing in the Stock Market. Apply key stock investing strategies such as investing in Dividend Paying Stocks, Growth Stocks & Value Stocks. Understand how to read market cycles and overcome the fear of losing money.",
+		regFee: "₹499",
+		contact: [
+			{
+				name: "Nehal",
+				mobile: "+91 99417 50200",
+			},
+		],
+	},
 
-  // The Virtual Reality
-  {
-    title: "The Virtual Reality",
-    url: "virtual-reality",
-    img: imageBasePath + "virtual-reality/logo.png",
-    speakers: [
-      {
-        name: "XR Labs",
-        company: "An AR VR company",
-        image: imageBasePath + "virtual-reality/xr_labs.png",
-      },
-    ],
-    time: "9:00 AM",
-    date: "March 31, 2022",
-    venue: "ECE Department",
-    oneLiner:
-      "Learn about the metaverse from scratch through group discussions.",
-    description:
-      "Corona has made a huge impact on work environment. The whole office, college and school have shrunk into a laptop and phone. Now it’s time for a revolution to bring a new world into one’s home. Want to know more about Facebook’s new name ‘META’?! Want to be the creator of the Virtual universe?! Here is the right opportunity for you to learn metaverse from scratch through group discussions and fun interactive sessions. XR Labs is digital experiences company that has done significant work in Augmented reality, Virtual reality and mixed reality and also 100+ successful projects",
-    regFee: "₹599 (including taxes)",
-    contact: [
-      {
-        name: "Dharaneesh",
-        mobile: "+91 95859 61212",
-      },
-    ],
-  },
+	// The Virtual Reality
+	{
+		title: "The Virtual Reality",
+		url: "virtual-reality",
+		img: imageBasePath + "virtual-reality/logo.png",
+		speakers: [
+			{
+				name: "XR Labs",
+				company: "An AR VR company",
+				image: imageBasePath + "virtual-reality/xr_labs.png",
+			},
+		],
+		time: "9:00 AM",
+		date: "March 31, 2022",
+		venue: "ECE Department",
+		oneLiner:
+			"Learn about the metaverse from scratch through group discussions.",
+		description:
+			"Corona has made a huge impact on work environment. The whole office, college and school have shrunk into a laptop and phone. Now it’s time for a revolution to bring a new world into one’s home. Want to know more about Facebook’s new name ‘META’?! Want to be the creator of the Virtual universe?! Here is the right opportunity for you to learn metaverse from scratch through group discussions and fun interactive sessions. XR Labs is digital experiences company that has done significant work in Augmented reality, Virtual reality and mixed reality and also 100+ successful projects",
+		regFee: "₹599 (including taxes)",
+		contact: [
+			{
+				name: "Dharaneesh",
+				mobile: "+91 95859 61212",
+			},
+		],
+	},
 
-  // Event Management
-  {
-    title: "Event Management",
-    url: "event-management",
-    img: imageBasePath + "event-management/logo.png",
-    speakers: [
-      {
-        name: "EEMA (Event and Entertainment Management Association)",
-        company: "The only national body",
-        image: imageBasePath + "event-management/eema.png",
-      },
-    ],
-    time: "10:00 AM",
-    date: "April 1, 2022",
-    venue: "ECE Department",
-    oneLiner:
-      "Get an insight into what goes behind during wedding planning, corporate functions etc.",
-    description:
-      "Get an insight into what goes behind during wedding planning, corporate functions, experimental marketing, brand activation, sports management and so on. To what extremes will managing an event take you to? Getting an order, making arrangements, running errands, completing an event and expanding your client base! Well, it isn't as easy as it might sound… Learn the nook and corner of Event Management from the best teams in the business. This will be a combined theory and hands on, fun interactive session.",
-    regFee: "₹300 (including taxes)",
-    contact: [
-      {
-        name: "Iniya D",
-        mobile: "+91 93449 28651",
-      },
-    ],
-  },
+	// Event Management
+	{
+		title: "Event Management",
+		url: "event-management",
+		img: imageBasePath + "event-management/logo.png",
+		speakers: [
+			{
+				name: "EEMA (Event and Entertainment Management Association)",
+				company: "The only national body",
+				image: imageBasePath + "event-management/eema.png",
+			},
+		],
+		time: "10:00 AM",
+		date: "April 1, 2022",
+		venue: "ECE Department",
+		oneLiner:
+			"Get an insight into what goes behind during wedding planning, corporate functions etc.",
+		description:
+			"Get an insight into what goes behind during wedding planning, corporate functions, experimental marketing, brand activation, sports management and so on. To what extremes will managing an event take you to? Getting an order, making arrangements, running errands, completing an event and expanding your client base! Well, it isn't as easy as it might sound… Learn the nook and corner of Event Management from the best teams in the business. This will be a combined theory and hands on, fun interactive session.",
+		regFee: "₹300 (including taxes)",
+		contact: [
+			{
+				name: "Iniya D",
+				mobile: "+91 93449 28651",
+			},
+		],
+	},
 
-  // Graphic Designing
-  {
-    title: "Graphic Designing",
-    url: "graphic-designing",
-    img: imageBasePath + "graphic-designing/logo.png",
-    speakers: [
-      {
-        name: "Boopathy Srinivasan",
-        company: "Faculty for Graphic designing at NIFT Chennai",
-        // image: imageBasePath + "graphic-designing/logo.png",
-      },
-    ],
-    time: "9:00 AM",
-    date: "April 1, 2022",
-    venue: "ECE Department",
-    oneLiner:
-      "Get an insight into what goes behind during wedding planning, corporate functions etc.",
-    description:
-      "Learn the basics of graphic designing through general group discussions. Get introduced to the Theoretical approach on software’s used in graphic designing and designing of logo. Hands on training on GIMP and PHOTOSHOP software’s. (NOTE: Please come with a laptop installed with GIMP & PHOTOSHOP Software to avail the hands on experience with graphic designing software.)",
-    regFee: "₹360 (including taxes)",
-    contact: [
-      {
-        name: "Akshay",
-        mobile: "+91 98410 44390",
-      },
-    ],
-  },
+	// Graphic Designing
+	{
+		title: "Graphic Designing",
+		url: "graphic-designing",
+		img: imageBasePath + "graphic-designing/logo.png",
+		speakers: [
+			{
+				name: "Boopathy Srinivasan",
+				company: "Faculty for Graphic designing at NIFT Chennai",
+				// image: imageBasePath + "graphic-designing/logo.png",
+			},
+		],
+		time: "9:00 AM",
+		date: "April 1, 2022",
+		venue: "ECE Department",
+		oneLiner:
+			"Learn the basics of graphic designing through general group discussions",
+		description:
+			"Learn the basics of graphic designing through general group discussions. Get introduced to the Theoretical approach on software’s used in graphic designing and designing of logo. Hands on training on GIMP and PHOTOSHOP software’s. (NOTE: Please come with a laptop installed with GIMP & PHOTOSHOP Software to avail the hands on experience with graphic designing software.)",
+		regFee: "₹360 (including taxes)",
+		contact: [
+			{
+				name: "Akshay",
+				mobile: "+91 98410 44390",
+			},
+		],
+	},
 ];
