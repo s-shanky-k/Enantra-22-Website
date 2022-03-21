5cc19577325fdb2acd5e3c98ec0bdc22bbb4a3b5
Shankar Kumar S <seshankar20@gmail.com>
2022-03-21 23:04:08 +0530

    Updated mini events data

--- src/data/MiniEvents.js
+++ src/data/MiniEvents.js
@@ -6,9 +6,10 @@ export const MINI_EVENTS = [
 		title: "Arena Hunt",
 		url: "arena-hunt",
 		img: imageBasePath + "arena-hunt/logo.png",
-		oneLiner: "Learn how to become an entrepreneur in Automotive service",
+		oneLiner:
+			"Based on ideas the participant can come up with for a given location",
 		description:
-			"Learn how to become an entrepreneur in Automotive service. Obtain in depth experience with the latest control systems and electronic components in cars. Explore the future of Autonomous cars, electric vehicles, hybrid vehicles and hydrogen cell cars and its practical applications with hands on training session.",
+			"The event is based on ideas the participant can come up with for a given location. More than the idea, the location and the behaviour of the people in those particular surrounding influences a lot more in making that idea or start up a success. This event may help in making people think based on the location more than just focusing on the idea itself.",
 		time: "9am-10am",
 		date: "31/3/2022",
 		venue: "KP-408",
@@ -25,16 +26,17 @@ export const MINI_EVENTS = [
 		title: "Save the Company!",
 		url: "save-the-company",
 		img: imageBasePath + "save-the-company/logo.png",
-		oneLiner: "Some one liner about this event will be cool",
+		oneLiner:
+			"Aim of this event is inspire to have a different way of strategic planning",
 		description:
-			"Learn how to become an entrepreneur in Automotive service. Obtain in depth experience with the latest control systems and electronic components in cars. Explore the future of Autonomous cars, electric vehicles, hybrid vehicles and hydrogen cell cars and its practical applications with hands on training session.",
+			"Aim of this event is inspire to have a different way of strategic planning and impress upon the ideas which can be get rid of; the participants come up with some realistic and creative problems as well as an acceptable approach to solve it.",
 		time: "10am-11am",
 		date: "31/3/2022",
 		venue: "KP-412",
 		regFee: "Rs. 99",
 		contact: [
 			{
-				name: "Name needed",
+				// name: "Name needed",
 				mobile: "+91 86108 07838",
 			},
 		],
@@ -45,16 +47,17 @@ export const MINI_EVENTS = [
 		title: "Brand Race",
 		url: "brand-race",
 		img: imageBasePath + "brand-race/logo.png",
-		oneLiner: "Some one liner about this event will be cool",
+		oneLiner:
+			"An event to share your knowledge and to explore more about branding",
 		description:
-			"Learn how to become an entrepreneur in Automotive service. Obtain in depth experience with the latest control systems and electronic components in cars. Explore the future of Autonomous cars, electric vehicles, hybrid vehicles and hydrogen cell cars and its practical applications with hands on training session.",
+			"The brand race is an event to share your knowledge and to explore more about branding. It consists of a qualifying round and a final round. The qualifying round will be conducted as a quiz among 5 teams for each run-through and it comprises a minimum of 5 run-throughs (according to the members count and time). The final round will be a presentation round dealing with rebranding. Juries’ decision will be final.",
 		time: "11am-12pm",
 		date: "31/3/2022",
 		venue: "KP-410",
 		regFee: "Rs. 99",
 		contact: [
 			{
-				name: "Name needed",
+				// name: "Name needed",
 				mobile: "+91 97890 02028",
 			},
 		],
@@ -65,16 +68,17 @@ export const MINI_EVENTS = [
 		title: "Shark Tank",
 		url: "shark-tank",
 		img: imageBasePath + "shark-tank/logo.png",
-		oneLiner: "Some one liner about this event will be cool",
+		oneLiner:
+			"A real-world experience to pitch their start up ideas in front of investors",
 		description:
-			"Learn how to become an entrepreneur in Automotive service. Obtain in depth experience with the latest control systems and electronic components in cars. Explore the future of Autonomous cars, electric vehicles, hybrid vehicles and hydrogen cell cars and its practical applications with hands on training session.",
+			"This mini event aims to give the participants a real-world experience to pitch their start up ideas in front of investors. This platform will help share the participant’s start-up ideas and help them refine their ideas with the feedback that they receive. It also gives them an experience of being a “Shark” who decides whether or not to invest in a company. This event can turn out to be a fun group discussion among young budding entrepreneurs.",
 		time: "12pm-1pm",
 		date: "31/3/2022",
 		venue: "KP-409",
 		regFee: "Rs. 99",
 		contact: [
 			{
-				name: "Name needed",
+				// name: "Name needed",
 				mobile: "+91 95000 99711",
 			},
 		],
@@ -85,16 +89,17 @@ export const MINI_EVENTS = [
 		title: "Enviropreneur",
 		url: "enviropreneur",
 		img: imageBasePath + "enviropreneur/logo.png",
-		oneLiner: "Some one liner about this event will be cool",
+		oneLiner:
+			"Find alternate solutions to currently used products which cause environmental problems",
 		description:
-			"Learn how to become an entrepreneur in Automotive service. Obtain in depth experience with the latest control systems and electronic components in cars. Explore the future of Autonomous cars, electric vehicles, hybrid vehicles and hydrogen cell cars and its practical applications with hands on training session.",
+			"The event aims to kindle interest of young entrepreneurs to find alternate products/ solutions to currently widely used products which cause environmental problems. An eco-friendly turn towards everyday products that cause more harm to the Earth than we know!",
 		time: "1pm-2pm",
 		date: "31/3/2022",
 		venue: "KP-311",
 		regFee: "Rs. 99",
 		contact: [
 			{
-				name: "Name needed",
+				// name: "Name needed",
 				mobile: "+91 91504 47057",
 			},
 		],
@@ -105,16 +110,16 @@ export const MINI_EVENTS = [
 		title: "Entrevert",
 		url: "entrevert",
 		img: imageBasePath + "entrevert/logo.png",
-		oneLiner: "Some one liner about this event will be cool",
+		oneLiner: "An event test your entrepreneurship skills",
 		description:
-			"Learn how to become an entrepreneur in Automotive service. Obtain in depth experience with the latest control systems and electronic components in cars. Explore the future of Autonomous cars, electric vehicles, hybrid vehicles and hydrogen cell cars and its practical applications with hands on training session.",
+			"Food for thought. Have you ever wondered who rules the business world? It’s time to test your entrepreneurship skills! We at AUSEC are here to help you!We bring to you this event with 2 exciting rounds which is a brainstorming yet rejoicing session.Keep winning tickets as you progress with each round. Rules: Come join us with a team of 3 or a team of 4. All you will be needing is a creative mind. What are you waiting for? Let your inner Genie run wild!",
 		time: "2pm-3pm",
 		date: "31/3/2022",
 		venue: "KP-411",
 		regFee: "Rs. 99",
 		contact: [
 			{
-				name: "Name needed",
+				// name: "Name needed",
 				mobile: "+91 86108 07838",
 			},
 		],
@@ -125,11 +130,11 @@ export const MINI_EVENTS = [
 		title: "Poster Mania",
 		url: "poster-mania",
 		img: imageBasePath + "poster-mania/logo.png",
-		oneLiner: "Some one liner about this event will be cool",
+		oneLiner: "Advertise a product by making a poster",
 		description:
-			"Learn how to become an entrepreneur in Automotive service. Obtain in depth experience with the latest control systems and electronic components in cars. Explore the future of Autonomous cars, electric vehicles, hybrid vehicles and hydrogen cell cars and its practical applications with hands on training session.",
+			"Are you someone who is capable of advertising a product by making a poster. This is the right platform for you. THIS EVENT IS FREE FOR THE SUBMISSIONS BEFORE 20TH MARCH, so send in your creativity before the clock runs out. Time comes with a price, results come with a prize. Send your entries as .jpg or .png file to postermaniaenantra@gmail.com along with your name, department, year and college.",
 		// time: "TBD",
-		date: "31/3/2022",
+		date: "Last Date - 25/3/2022",
 		venue: "Online Event",
 		regFee: "Rs. 50",
 		// contact: [
@@ -145,11 +150,11 @@ export const MINI_EVENTS = [
 		title: "Creative Photography",
 		url: "creative-photography",
 		img: imageBasePath + "creative-photography/logo.png",
-		oneLiner: "Some one liner about this event will be cool",
+		oneLiner: "You don't take a photograph, you make it",
 		description:
-			"Learn how to become an entrepreneur in Automotive service. Obtain in depth experience with the latest control systems and electronic components in cars. Explore the future of Autonomous cars, electric vehicles, hybrid vehicles and hydrogen cell cars and its practical applications with hands on training session.",
+			"'You don't take a photograph, you make it'. Send us a photograph you have taken, of a product of your choice! And win exciting cash prizes!! Theme - BLACK AND WHITE PHOTOGRAPHY. Send your entries as .jpg or .png file to enantraretrophoto@gmail.com along with your name, department, year and college. Last date for submission March 25 2022",
 		// time: "TBD",
-		date: "31/3/2022",
+		date: "Last Date - 25/3/2022",
 		venue: "Online Event",
 		regFee: "Free",
 		// contact: [
